49f685da0bcf1de9b5ec4801a44822adc65684f6
Oscar Tandioy <tato.tandioy@gmail.com>
2023-02-27 21:06:38 -0500

    docs: added documentacion in publications, post, get id, delete id

--- docs/routesDocs/cities.docs.js
+++ docs/routesDocs/cities.docs.js
@@ -1,26 +1,27 @@
 /**
  * @openapi
- * /api/v1/cities:
- *   get:
- *     security:
- *       - bearerAuth: []
- *     summary: Only users for Administrators
- *     tags:
- *       - Cities
- *     parameters:
- *       - in: query
- *         name: page
- *         type: integer
- *         example: 1
- *       - in: query
- *         name: size
- *         type: integer
- *         example: 2
- *     responses:
- *       200:
- *         description: Ok
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/responses/responseGetAllCities'
+ * paths:
+ *   /api/v1/cities:
+ *     get:
+ *       security:
+ *         - bearerAuth: []
+ *       summary: For all tokenized users
+ *       tags:
+ *         - Cities
+ *       parameters:
+ *         - in: query
+ *           name: page
+ *           type: integer
+ *           example: 1
+ *         - in: query
+ *           name: size
+ *           type: integer
+ *           example: 2
+ *       responses:
+ *         200:
+ *           description: Ok
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/responses/responseGetAllCities'
  */
--- docs/schemas/publications.schema.js
+++ docs/schemas/publications.schema.js
@@ -126,4 +126,81 @@
  *                               name:
  *                                 type: string
  *                                 example: Conciertos y Musica
+ *       responseGetByIdPublication:
+ *         type: object
+ *         properties:
+ *           result:
+ *             type: object
+ *             properties:
+ *               id:
+ *                 type: string
+ *                 example: d116979e-130f-442c-b8d5-7f1350747d42
+ *               title:
+ *                 type: string
+ *                 example: Videos Games
+ *               description:
+ *                 type: string
+ *                 example: All kinds of video games in general
+ *               votes_count:
+ *                 type: integer
+ *                 example: 2
+ *               user:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: string
+ *                     example: a7da2813-cf22-4c10-b682-45da5553d5f4
+ *                   first_name:
+ *                     type: string
+ *                     example: Oscar
+ *               cities:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: string
+ *                     example: e89243c8-1673-442f-8d4d-86fa460cdcc4
+ *                   state_id:
+ *                     type: string
+ *                     example: 41f31935-cd9b-4920-9751-60b3e09be838
+ *                   name:
+ *                     type: string
+ *                     example: Videos Games
+ *               publication_type:
+ *                 type: object
+ *                 id:
+ *                   type: string
+ *                   example: cc95b8c2-a4fd-4323-9273-a239dcb42cfd
+ *                 name:
+ *                   type: string
+ *                   example: Videos Games
+ *               tags:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     example: 2
+ *                   tag:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                         example: 2
+ *                       name:
+ *                         type: string
+ *                         example: Video Games
+ *               votes:
+ *                 type: array
+ *                 items:
+ *                   oneOf:
+ *                   - type: object
+ *                     properties:
+ *                       id:
+ *                         type: string
+ *                         example: 2dc095e3-ba31-4168-9aa8-beed59d11830
+ *                       publication_id:
+ *                         type: strict
+ *                         example: d116979e-130f-442c-b8d5-7f1350747d42
+ *                       user_id:
+ *                         type: string
+ *                         example: a7da2813-cf22-4c10-b682-45da5553d5f4
  */
