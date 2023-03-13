46c70e29374c58566465b2c8a3dde64dfdfa302d
Oscar Tandioy <tato.tandioy@gmail.com>
2023-03-12 21:06:49 -0500

    perf: Removed unnecessary model section

--- database/models/users_tags.js
+++ database/models/users_tags.js
@@ -18,8 +18,16 @@ module.exports = (sequelize, DataTypes) => {
   }
   users_tags.init(
     {
-      tag_id: DataTypes.INTEGER,
-      user_id: DataTypes.UUID,
+      tag_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+      },
+      user_id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        primaryKey: true
+      },
     },
     {
       sequelize,
